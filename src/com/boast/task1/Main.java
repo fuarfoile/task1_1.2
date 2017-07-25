64a7a4c174414f05056df73bb8cb65bf9203604c
Oleh Bondarenko <boa.st@ya.ru>
2017-07-25 14:06:34 +0300

    Add files via upload

---
+++ src/com/boast/task1/Main.java
@@ -0,0 +1,44 @@
+package com.boast.task1;
+
+import java.util.Scanner;
+
+/**
+ * Created by Bondarenko Oleh on 22.07.2017.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        long maxNumber = in.nextLong();
+
+        getPerfectNumbers(maxNumber);
+
+    }
+
+    private static void getPerfectNumbers(long maxNumber) {
+
+        for (int p = 1; p < 31; p++) {
+            long candidateForPrime = (2 << p) - 1;
+
+            if (primeTest(candidateForPrime)) {
+                long perfectNumber = candidateForPrime * (2 << (p - 1));
+                if (perfectNumber > maxNumber) {
+                    break;
+                }
+                System.out.println(perfectNumber);
+            }
+        }
+    }
+
+    private static boolean primeTest(long n){
+
+        for(long i = 2; i <= Math.sqrt(n); i++){
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
